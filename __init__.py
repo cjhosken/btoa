edba77d39d18d7c9f60e8c263ff32a464a06a5c2
Christopher Hosken <hoskenchristopher@gmail.com>
2025-05-25 16:52:04 +0000

    addon development

---
+++ __init__.py
@@ -0,0 +1,34 @@
+import bpy, os, sys
+
+bl_info = {
+    "name" : "Blender to Arnold",
+    "author" : "Christopher Hosken",
+    "version" : (7, 4, 2, 0),
+    "blender" : (4, 4, 0),
+    "description" : "Autodesk's Arnold Renderer integration into Blender",
+    "warning" : "This Addon is currently under development",
+    "tracker_url":"",
+    "doc_url":"",
+    "community":"",
+    "downloads":"",
+    "main_web":"",
+    "support": "COMMUNITY",
+    "category" : "Render"
+}
+
+from . import properties, engine, preferences, operators, ui
+
+
+def register():
+    properties.register()
+    preferences.register()
+    engine.register()
+    operators.register()
+    ui.register()
+
+def unregister():
+    ui.unregister()
+    operators.unregister()
+    engine.unregister()
+    preferences.unregister()
+    properties.unregister()
